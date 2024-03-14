1c155fb0819cdfb064b6686bf749ddc2ee1f759f
Eric <ericnob1974@gmail.com>
2024-03-14 20:16:45 -0300

    Lab 01 Ej 2

--- lab-01/sum.c
+++ lab-01/sum.c
@@ -5,6 +5,19 @@ int main(int argc, char *argv[])
 {
     // Agregar código aquí.
 
+    int i=1;
+    int sum=0;
+    for (i;i<argc;i++){
+	if ((*argv[i] >= '0') && (*argv[i] <= '9')){
+    		sum += atoi(argv[i]);
+        }
+        else {
+        	printf("Debe ingresar unicamente numeros\n");
+		exit(EXIT_FAILURE);
+        }
+    }
+    printf("El resultado de la suma es: %d\n",sum);
+    
     // Termina la ejecución del programa.
     exit(EXIT_SUCCESS);
 }
