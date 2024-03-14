dc73814ee6261401b5b63283189f00fbff0be332
Eric <ericnob1974@gmail.com>
2024-03-14 17:48:35 -0300

    Lab 01 Ej 1

--- lab-01/hola.c
+++ lab-01/hola.c
@@ -3,9 +3,13 @@
 
 int main(int argc, char *argv[])
 {
+    printf("Cadena pasada por el usario: ");
     // Agregar código aquí.
-    printf("hola\n");
-
+    int i=1;
+    for (i;i<argc;i++){
+        printf("%s ", argv[i]);
+    }
+    printf("\nCantidad de divisiones del array de punteros: %d\n",argc);
     // Termina la ejecución del programa.
     exit(EXIT_SUCCESS);
 }
