7852699c7dbcb247cbc49a35a40cbeb05c0870c1
Eric <ericnob1974@gmail.com>
2024-03-16 02:41:45 -0300

    Lab 01 Ej 3

--- lab-01/palabras.c
+++ lab-01/palabras.c
@@ -4,7 +4,16 @@
 int main(int argc, char *argv[])
 {
     // Agregar código aquí.
-
+    int c = 0;
+    char palabras[100];
+    printf("Ingrese una frase (O control + c para salir)\n");
+    while(1){
+	scanf("%s",palabras);
+	if ((c = getchar()) != EOF){
+		printf("%s\n", palabras);   		
+        }
+    }
+    
     // Termina la ejecución del programa.
     exit(EXIT_SUCCESS);
 }
